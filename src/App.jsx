c2bbd76140a4b6d724feb4c75f48e728bcc167aa
Daud Abdiwahab Elmoge <daudwahab1320@gmail.com>
2025-04-14 14:39:18 +0300

    added comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-import { useState } from "react";
+import { useState } from "react"; // Importing useState from React
 import "./App.css";
 
 function App() {
   const [formdata, setFormdata] = useState({
+    // Initializing state with useState
+    // Setting initial state for form data
+    // The state is an object with two properties: name and password
+    // Both properties are initialized to empty strings
+    // This state will be used to store the values of the input fields
+    // as the user types in their information
     name: "",
     password: "",
   });
 
   const handleChange = (e) => {
+    // Function to handle changes in input fields
+    // This function is called whenever the user types in the input fields
+    // It updates the state with the new value of the input field
+    // The event object (e) contains information about the input field that triggered the change
+    // We use destructuring to extract the name and value properties from the event target
     const { name, value } = e.target;
     setFormdata((prevData) => ({
       ...prevData,
@@ -15,13 +26,19 @@ function App() {
     }));
   };
 
-   const handleSubmit = (e) => {
+  const handleSubmit = (e) => {
+    // Function to handle form submission
+    // This function is called when the user submits the form
+    // It prevents the default behavior of the form submission (which would refresh the page)
+    // and logs the form data to the console
     e.preventDefault();
-     console.log('Form Data Submitted:');
-    };
+    console.log("Form Data Submitted:");
+  };
   return (
     <>
       <form onSubmit={handleSubmit}>
+        {/* The onSubmit event is triggered when the user submits the form */}
+        {/* The handleSubmit function is called to handle the form submission */}
         <h2>Sign Up</h2>
         <label>
           Name:
